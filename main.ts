3dad4b0df0e89a172e9fdeaa10bfd2cb50fcee33
DimitrisGiuliano <132891674+DimitrisGiuliano@users.noreply.github.com>
2026-03-11 16:21:42 +0200

    Return acceleration instead of current speed

--- main.ts
+++ main.ts
@@ -119,7 +119,7 @@ export function acceleration(): number {
     prevTotalDegrees = currentDegrees
     prevTime = currentTime
 
-    return currentSpeed
+    return accel
 }
 
     //% block="AS5600 change power mode %mode"
